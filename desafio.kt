2f39bf4ba8c8b6b6b70378499f4146cd47f9b806
Renan Cunha <renancdev@gmail.com>
2024-11-04 20:25:42 -0300

    Métodos para matricular, procurar e remover usarios adicionados. Nivel da formação alterado

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,72 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario(val id:Int, var nome: String)
 
-class Usuario
+data class ConteudoEducacional(var nome: String, var duracao: Int = 60)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Formacao(var nome: String, var conteudos: List<ConteudoEducacional>, var niveis: Int) {
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
+    private val inscritos = mutableListOf<Usuario>()
+    
+    fun mostraInscritos() = inscritos.toString()
+    
+   	fun procuraInscrito(usuario: Usuario): Any{
+        if(usuario in inscritos){
+            return usuario
+        } else {
+            return "Usuario não encontrado"
+        }
+    }
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        
+        println("Usuario ${usuario.nome} adicionado com sucesso")
+    }
+    
+    fun removerInscrito(usuario: Usuario){
+        if(usuario in inscritos){
+        	inscritos.remove(usuario)   
+            println("Usuario ${usuario.nome} foi removido com sucesso")
+        } else{
+            println("Este usuario não está inscrito na formação")
+        }
+       
+        
+    }
+
+    fun nivel():String{
+        when(niveis){
+            1 -> return "Basico"
+            2 -> return "Intermediario"
+            3 -> return "Avancado"
+            else-> return "Nivel não selecionado"
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val ronaldo = Usuario(0, "Ronaldo")
+	val jaime = Usuario(1, "jaime")
+    
+    val java = ConteudoEducacional("Curso Java")
+    val kotlin = ConteudoEducacional("Curso Kotlin", 68)
+
+    var listaConteudo: List<ConteudoEducacional> = listOf(java, kotlin)
+
+    var devKotlin = Formacao("Formação Kotlin",listaConteudo, 2)
+    
+    devKotlin.matricular(ronaldo)
+    println(devKotlin.procuraInscrito(ronaldo))
+    println(devKotlin.procuraInscrito(jaime))
+    devKotlin.matricular(jaime)
+    
+    println(devKotlin.mostraInscritos())
+    
+    devKotlin.removerInscrito(jaime)
+    devKotlin.removerInscrito(jaime)
+    
+    println(devKotlin.nivel())
+    
 }
